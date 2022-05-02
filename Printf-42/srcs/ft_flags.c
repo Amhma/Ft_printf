c2d31ecb9cb94e9d63221aeee40d302fcf8ca1bb
Amir Mahla <ammah@MacBook-Pro-de-Amir-2.local>
2022-05-02 20:34:37 +0200

    Ft_printf : update ft_flag.c

--- Printf-42/srcs/ft_flags.c
+++ Printf-42/srcs/ft_flags.c
@@ -10,26 +10,6 @@ char	*ft_flag_zero(t_flag *fg, int option)
 		new = ft_calloc(fg->width + 1, sizeof(char));
 		ft_memset(new, '0', fg->width);
 	}
-	else if (fg->zero && fg->point && fg->width > fg->precision
-		&& fg->width > fg->nbrlen + option)
-	{
-		new = ft_calloc(fg->width + 1, sizeof(char));
-		ft_memset(new, ' ', fg->width);
-	}
-	return (new);
-}
-
-char	*ft_flag_minus(t_flag *fg, int option)
-{
-	char	*new;
-
-	new = NULL;
-	if (fg->minus && (!(fg->point) || (fg->point && fg->width > fg->precision))
-		&& fg->width > fg->nbrlen + option)
-	{
-		new = ft_calloc(fg->width + 1, sizeof(char));
-		ft_memset(new, ' ', fg->width);
-	}
 	return (new);
 }
 
@@ -51,16 +31,14 @@ char	*ft_flag(t_flag *fg, int option)
 	char	*str;
 
 	str = NULL;
-	if (!(fg->zero) && !(fg->minus) && fg->width > fg->precision
+	if (fg->zero)
+		str = ft_flag_zero(fg, option);
+	if (!str && fg->width > fg->precision
 		&& fg->width > fg->nbrlen + option)
 	{
 		str = ft_calloc(fg->width + 1, sizeof(char));
 		ft_memset(str, ' ', fg->width);
 	}
-	if (fg->zero)
-		str = ft_flag_zero(fg, option);
-	if (fg->minus)
-		str = ft_flag_minus(fg, option);
 	if (fg->point)
 		ft_flag_point(fg, &str, option);
 	return (str);
